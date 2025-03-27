1ba864b8cc66f9f3626cfce2a33fdea423f11770
Mario da Graca <mario.dagraca@gmx.de>
2025-03-27 22:49:27 +0100

    Get W&B Project and Run Name from kwargs if available

--- rfdetr/detr.py
+++ rfdetr/detr.py
@@ -71,7 +71,7 @@ class RFDETR:
         self.callbacks["on_fit_epoch_end"].append(metrics_tensor_board_sink.update)
         self.callbacks["on_train_end"].append(metrics_tensor_board_sink.close)
 
-        metrics_wandb_sink = MetricsWandBSink(output_dir=config.output_dir, project_name = None, run_name = None, config = all_kwargs)
+        metrics_wandb_sink = MetricsWandBSink(output_dir=config.output_dir, project_name = kwargs.get("project_name", None), run_name = kwargs.get("run_name", None), config = all_kwargs)
         self.callbacks["on_fit_epoch_end"].append(metrics_wandb_sink.update)
         self.callbacks["on_train_end"].append(metrics_wandb_sink.close)
 
