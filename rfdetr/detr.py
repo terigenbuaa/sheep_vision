correct calculation for width, height

rfdetr/detr.py
@@ -137,12 +137,13 @@ class RFDETR:
                 img_tensor = img
                 assert img_tensor.shape[0] == 3, "image must have 3 channels"
 
+            h, w = img_tensor.shape[1:]
+            orig_sizes.append((h, w))
+
             img_tensor = img_tensor.to(self.model.device)
             img_tensor = F.normalize(img_tensor, self.means, self.stds)
             img_tensor = F.resize(img_tensor, (self.model.resolution, self.model.resolution))
 
-            h, w = img_tensor.shape[1:]
-            orig_sizes.append((h, w))
             processed_images.append(img_tensor)
 
         batch_tensor = torch.stack(processed_images)